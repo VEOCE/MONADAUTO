422528540aebb4b312c139519f8f9db739a6af25
VEOCE <136567088+VEOCE@users.noreply.github.com>
2025-03-21 00:13:29 +0700

    Update bebop.py

--- modules/bebop.py
+++ modules/bebop.py
@@ -11,7 +11,7 @@ load_dotenv()
 def display_header():
     print(Style.BRIGHT + Fore.CYAN + "======================================")
     print(Style.BRIGHT + Fore.CYAN + "            Bebop Bot                 ")
-    print(Style.BRIGHT + Fore.CYAN + "====================================\n")
+    print(Style.BRIGHT + Fore.CYAN + "======================================\n")
 
 display_header()
 
